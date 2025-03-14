3c2b203829fd4aa5733e058cb5270bacfe4ef841
Kevin Thomas <kevin.1989.thomas@gmail.com>
2025-03-14 15:51:13 -0400

    chore: update license year

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Elevation Beats Inc
+ * Copyright 2025 Elevation Beats Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,4 +102,3 @@ export type ClassGroupEntry = [
   ClassGroupName: string,
   PricePerClass: number,
 ];
-  
